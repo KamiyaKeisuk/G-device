9fb25a3f76fcf2ecf350848db75eb7cb675a0e4e
KamiyaKeisuk <biotite078260@gmail.com>
2021-12-09 03:20:21 +0000

    [add]admin_device

--- app/controllers/admin/devices_controller.rb
+++ app/controllers/admin/devices_controller.rb
@@ -1,2 +1,46 @@
 class Admin::DevicesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @devices = Device.page(params[:page]).per(10)
+  end
+
+  def new
+    @device = Device.new
+    @categories = Category.all
+    @makers = Maker.all
+  end
+
+  def create
+    @device = Device.new(device_params)
+    if @device.save
+      redirect_to admin_device_path(@device)
+    else
+      redirect_to request.referer
+    end
+  end
+
+  def show
+    @device = Device.find(params[:id])
+  end
+
+  def edit
+    @device = Device.find(params[:id])
+    @categories = Category.all
+    @makers = Maker.all
+  end
+
+  def update
+    @device = Device.find(params[:id])
+    if @device.update(device_params)
+      redirect_to admin_device_path(@device)
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+    def device_params
+      params.require(:device).permit(:category_id, :maker_id, :name, :description, :price, :image)
+    end
 end
