2f860820cb1837b1c3783daee215d20b48cdde52
KamiyaKeisuk <biotite078260@gmail.com>
2021-12-13 07:26:58 +0000

    key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '52177d4a7e18e5138613b50ae2e42f03ffe0259076aba0015e56207ad328cc369a47b1a99f1979abc3aa6755f28a6b3e9831e436047748fcac40e2dd02062b96'
