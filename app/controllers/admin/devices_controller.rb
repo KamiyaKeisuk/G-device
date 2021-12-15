051f5947124ce87927fb216879064e151dc6fbbf
KamiyaKeisuk <biotite078260@gmail.com>
2021-12-15 08:58:56 +0000

    [add]header_footer

--- app/controllers/admin/devices_controller.rb
+++ app/controllers/admin/devices_controller.rb
@@ -3,6 +3,16 @@ class Admin::DevicesController < ApplicationController
 
   def index
     @devices = Device.page(params[:page]).per(10)
+    #選択したカテゴリーのデバイス一覧
+    if params[:category_id].present?
+      @category = Category.find(params[:category_id])
+      @devices = @category.devices.page(params[:page]).per(10)
+    #選択したメーカーのデバイス一覧
+    elsif
+      params[:maker_id].present?
+      @maker = Maker.find(params[:maker_id])
+      @devices = @maker.devices.page(params[:page]).per(10)
+    end
   end
 
   def new
