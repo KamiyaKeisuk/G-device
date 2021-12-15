83be5cfd78b76bf64a46617cd403aed796755399
KamiyaKeisuk <biotite078260@gmail.com>
2021-12-15 06:59:24 +0000

    [add]customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,29 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @customers = Customer.page(params[:page]).per(10)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :email, :is_quited)
+    end
 end
--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,2 +1,35 @@
 class Customer::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_mypage_path
+    else
+      redirect_to request.referer
+    end
+  end
+
+  def quit
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_quited: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+  	def customer_params
+  		params.require(:customer).permit(:name, :email, :is_quited)
+  	end
 end
